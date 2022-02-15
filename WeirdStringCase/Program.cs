d23d5ba1bbd52a3d3e2f6f3119ac9bd05d939808
Alan <isaev3658@gmail.com>
2022-02-15 16:20:29 +0600

    new exercises

---
+++ WeirdStringCase/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+string ToWeirdCase(string s)
+{
+    List<string> words = new List<string>(s.Split(' '));
+    string res = "";
+
+    for (int i = 0; i < words.Count; i++)
+    {
+        words[i] = words[i].ToLower();
+    }
+
+    foreach (string item in words)
+    {
+        if (item != words[0])
+        {
+            res += " ";
+        }
+        for (int i = 0; i < item.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                res += item[i].ToString().ToUpper();
+            }
+            else
+            {
+                res += item[i];
+            }
+        }
+    }
+    
+    //res = res.Trim(' ');
+    return res;
+}
+
+Console.Write(ToWeirdCase("Weird string case"));
