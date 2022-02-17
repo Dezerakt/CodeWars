d23da55012b940073b8c62633fe8108591abc870
Alan <isaev3658@gmail.com>
2022-02-17 15:09:03 +0600

    another exercices

---
+++ DuplicateEncoder/Program.cs
@@ -0,0 +1,36 @@
+﻿static string DuplicateEncode(string word)
+{
+    word = word.ToLower();
+    Dictionary<char, int> char_dict = new Dictionary<char, int>();
+    string res = string.Empty;
+
+    foreach (char item in word)
+    {
+        if (char_dict.ContainsKey(item))
+        {
+            char_dict[item] = char_dict[item] + 1;
+        }
+        else
+        {
+            char_dict.Add(item, 1);
+        }
+    }
+
+    foreach (var item in word)
+    {
+        if (char_dict[item] == 1)
+        {
+            res += "(";
+        }
+        else if (char_dict[item] > 1)
+        {
+            res += ")";
+        }
+    }
+
+    
+    return res;
+}
+
+string str = "DoneD";
+Console.WriteLine(DuplicateEncode(str));
