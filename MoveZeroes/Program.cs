0047a45018961d86549ba069687b3d00a2bbbf82
Alan <isaev3658@gmail.com>
2022-02-22 19:20:03 +0600

    another exercises

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿static int[] MoveZeroes(int[] arr)
+{
+    List<int> new_arr = new List<int>();
+    int nulls = 0;
+    foreach(int i in arr)
+    {
+        if(i != 0)
+        {
+            new_arr.Add(i);
+        }
+        else
+        {
+            nulls++;
+        }
+    }
+    for (int i = 0; i < nulls; i++)
+    {
+        new_arr.Add(0);
+    }
+
+
+    return new_arr.ToArray();
+}
+
+int[] arr = new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+foreach (int item in MoveZeroes(arr))
+{
+    Console.WriteLine(item);
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,24 @@
+﻿static int[] TwoSum(int[] numbers, int target)
+{
+    List<int> result = new List<int>();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        for (int a = 0; a < numbers.Length; a++)
+        {
+            if(numbers[i] + numbers[a] == target && i != a)
+            {
+                result.Add(i);
+                result.Add(a);
+                return result.ToArray();
+            }
+        }
+    }
+    return null;
+}
+
+int[] param = new int[] { 5, 2, 3, 5};
+
+foreach (var item in TwoSum(param, 10))
+{
+    Console.WriteLine(item);
+}
